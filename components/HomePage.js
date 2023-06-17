4a300c50453c32206889a5b4f5d52c237b5409ed
Majid Rahimi <majid88rahimi@gmail.com>
2023-06-17 12:34:15 +0300

    Added useState with no issue

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
+
 const Title = styled.h1`
-    color: red;
+    color: ${({$color}) => $color ? 'red' : 'blue'};
     font-size: 200px;
 `
 
 export default function HomePage() {
-    return <Title>Majid Rahimi</Title>
+    const [color, setColor] = useState(false)
+    
+    const handleOnClick = () => {
+        setColor(!color)
+    }
+
+    return <Title $color={color} onClick={handleOnClick}>Majid Rahimi</Title>
 }
